99089bd4d26f96a4f6a841144a7b3a5eeaf1dd7c
amrutha-varshini <40825488+amrutha-varshini@users.noreply.github.com>
2018-07-05 11:39:26 +0530

    adds break and continue with label

---
+++ keyword.java
@@ -0,0 +1,25 @@
+public class keyword
+{
+	public static void main(String[] args)
+	{
+		int[][] a= new int[3][3];
+		int i,j;
+		for(i=0;i<3;i++)
+			for(j=0;j<3;j++)
+				a[i][j]=i+j;
+		//break using label
+		nextloop:for(i=0;i<3;i++)
+			for(j=0;j<3;j++)
+				if(a[i][j]==3)
+					break nextloop;
+				else
+					System.out.println(a[i][j]+","+i+","+j);
+		//continue using label
+		nextloop:for(i=0;i<3;i++)
+			for(j=0;j<3;j++)
+				if(a[i][j]==3)
+					continue nextloop;
+				else
+					System.out.println(a[i][j]+","+i+","+j);
+	}
+}
