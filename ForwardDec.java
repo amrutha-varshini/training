e150b7a8f09d979f28f147bb7649d6880414237d
amrutha-varshini <40825488+amrutha-varshini@users.noreply.github.com>
2018-07-05 11:48:30 +0530

    adds an example to show no need of forward declaration

---
+++ ForwardDec.java
@@ -0,0 +1,13 @@
+public class ForwardDec
+{
+	public static void main(String[] args)
+	{
+		int a=10,b=5;
+		int c=add(a,b);
+		System.out.println(c);
+	}
+	static int add(int a, int b)
+	{
+		return a+b;
+	}
+}
