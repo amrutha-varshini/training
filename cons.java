8df3afc23411bfe851276cda3af6815875947183
amrutha-varshini <40825488+amrutha-varshini@users.noreply.github.com>
2018-07-06 15:13:40 +0530

    adds constructors

---
+++ cons.java
@@ -0,0 +1,30 @@
+public class cons
+{
+	public static void main(String[] args)
+	{
+		mathematics m1=new mathematics();
+		mathematics m2=new mathematics(2);
+		mathematics m3=new mathematics(2,3);
+		mathematics m4=new mathematics(2,3,4);//CTE
+	}
+}
+class mathematics
+{
+	int op1,op2;
+	mathematics ()
+	{
+		System.out.println("Inside default constructor");
+	}
+	mathematics (int a)
+	{
+		System.out.println("Inside parameterized constructor");
+	}
+	mathematics (int a,int b)
+	{
+		System.out.println("Inside overloaded constructor");
+	}
+	private mathematics(int a,int b, int c)
+	{
+		System.out.println("inside private constructor");
+	}
+}
