569e9f4c5df0cfb5e18faa2d488d801d623009c1
amrutha-varshini <40825488+amrutha-varshini@users.noreply.github.com>
2018-07-06 15:01:16 +0530

    adds access modifiers, packages and classes

---
+++ Access1/arithmetic2.java
@@ -0,0 +1,48 @@
+package Access1;
+
+public class arithmetic2
+{
+	private int a;
+	public int b;
+	int c;
+	protected int d;
+	private int add(int a, int b)
+	{
+		return a+b;
+	}
+	public int sub(int a, int b)
+	{
+		return a-b;
+	}
+	protected int mul(int a,int b)
+	{
+		return a*b;
+	}
+	int div(int a,int b)
+	{
+		return a/b;
+	}
+}
+class arithmetic3
+{
+	private int a;
+	public int b;
+	int c;
+	protected int d;
+	private int add(int a, int b)
+	{
+		return a+b;
+	}
+	public int sub(int a, int b)
+	{
+		return a-b;
+	}
+	protected int mul(int a,int b)
+	{
+		return a*b;
+	}
+	int div(int a,int b)
+	{
+		return a/b;
+	}
+}
