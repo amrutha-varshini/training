41adf0c70f837d3ae2ee807c34051439bec8c601
amrutha-varshini <40825488+amrutha-varshini@users.noreply.github.com>
2018-07-16 16:57:35 +0530

    adds abstract classes with lambda expressions

--- InterProb.java
+++ InterProb.java
@@ -3,22 +3,11 @@ abstract class B2
 {
 	abstract void show();
 }
-interface B1
-{
-	default void show()
-	{
-		System.out.println("hi");
-	}
-}
-class C extends B2 implements B1
-{
-	
-}
 public class InterProb
 {
 	public static void main(String[] args)
 	{
-		B1 c=new C();
+		B2 c=()->{System.out.println("hi");};
 		c.show();
 	}
 }
