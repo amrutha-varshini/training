7f21b6ff59eda3c9471a6afa573dfdd9364dc740
amrutha-varshini <40825488+amrutha-varshini@users.noreply.github.com>
2018-07-18 18:49:05 +0530

    adds method references

--- method.java
+++ method.java
@@ -3,32 +3,18 @@ public class method
 	public static void main(String[] args)
 	{
 		ExtendMe e=new ExtendMe();
-		ImplementMe a=ExtendMe::StatDisplay;
-		//ImplementMe b=ImplementMe::display;
-		ImplementMe c=e::display;
-		ImplementMe d=ExtendMe::new;
-		a.display();
-		//b.display();
-		c.display();
-		d.display();
+		ImplementMe b=ExtendMe::display;
+		b.display(e,3);
 	}
 }
 interface ImplementMe
 {
-	void display();
+	void display(ExtendMe a,int b);
 }
 class ExtendMe
 {
-	void display()
+	void display(int a)
 	{
 		System.out.println("Hello");
-	}
-	static void StatDisplay()
-	{
-		System.out.println("static Hello");
-	}
-	ExtendMe()
-	{
-		System.out.println("Constructor Hello");
 	}
 }
