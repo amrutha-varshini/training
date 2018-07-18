8579bec2b64fd65f316888b698e54d43bd850fbd
amrutha-varshini <40825488+amrutha-varshini@users.noreply.github.com>
2018-07-18 18:05:39 +0530

    adds generics

---
+++ GenTry1.java
@@ -0,0 +1,22 @@
+public class GenTry1
+{
+	public static void main(String[] args)
+	{
+		Container a=new Container();
+		A a1=new A1();
+		a.setObj(a1);
+	}
+}
+class Container 
+{
+	public <T>void setObj(T obj) 
+	{
+		System.out.println(obj.getClass().getName());
+	}
+}
+class A
+{
+}
+class A1 extends A
+{
+}
