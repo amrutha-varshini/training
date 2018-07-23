61b5a83ccceb9080f2b5ce43af28aab98b644e0a
amrutha-varshini <40825488+amrutha-varshini@users.noreply.github.com>
2018-07-23 12:22:48 +0530

    adds exception handling

--- except.java
+++ except.java
@@ -17,27 +17,8 @@ class except
 }
 class except1
 {
-	void test()// throws NoSuchFieldException
+	void test() throws NoSuchFieldException
 	{
-		try
-		{
-			
-			System.out.println("inside try1");
-			throw new SecurityException("its me");
-		}
-		catch (Exception e)
-		{
-			System.out.println("inside 1 catch");
-			System.out.println(e);
-			throw e;
-		}
-		finally
-		{
-			System.out.println("inside finally");
-			SecurityException e=new SecurityException();
-			System.out.println(e);
-			throw e;
-		}
-		//System.out.println("outside");
+		throw new NoSuchFieldException("its me");
 	}
 }
