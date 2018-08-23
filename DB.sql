db8a7fe5ff9d1e0885d3c52924eb87f941a240a3
amrutha-varshini <40825488+amrutha-varshini@users.noreply.github.com>
2018-08-23 20:23:19 +0530

    adds DB

--- DB.sql
+++ DB.sql
@@ -1,41 +1,36 @@
 create database mini_care;
 use mini_care;
-create table users(id int(10) not null auto_increment,
-					role char(6),
-					fname varchar(20) not null,
-					lname varchar(20) not null,
-					email varchar(20) not null,
-					pwd char(6) not null,
-					phno char(13) not null,
-					zip char(5) not null,
-					location address not null,
+create table users(id int(255) not null auto_increment,
+					role varchar(255),
+					fname varchar(255),
+					lname varchar(255),
+					email varchar(255)  ,
+					pwd varchar(255)  ,
+					phno varchar(255)  ,
+					zip varchar(255)  ,
+					address_line1 varchar(255)  ,
+					address_line2 varchar(255)  ,
+					city varchar(255)  ,
+					states varchar(255)  ,
 					last_active timestamp,
 					status enum("ACTIVE","INACTIVE"),
 					primary key(id));
 
-create table address(id int(10) not null, 
-					address_line1 varchar(20) not null,
-					address_line2 varchar(20) not null,
-					city varchar(20) not null,
-					states varchar(20) not null,
-					foreign key(id) references users(id),
-					primary key(id));
-
-create table seeker_job(id int(10) not null auto_increment,
-					userID int(10) not null,
-					title varchar(20) not null,
-					descr varchar(300) not null,
+create table seeker_job(id int(255) not null auto_increment,
+					userID int(255)  ,
+					title varchar(255)  ,
+					descr varchar(255)  ,
 					foreign key(userID) references users(id),
 					primary key(id));
 					
-create table sitter(id int(10) not null, 
-					title varchar(20),
-					descr varchar(300),
+create table sitter(id int(255) not null, 
+					title varchar(255),
+					descr varchar(255),
 					foreign key(id) references users(id),
 					primary key(id));
 	
-create table job_status(seekerid int(10) not null,
-						userid int(10) not null,
+create table job_status(jobid int(255) not null,
+						userid int(255) not null,
 						foreign key(userid) references users(id),
-						foreign key(seekerid) references seeker_job(id),
-						primary key(userid,seekerid));
+						foreign key(jobid) references seeker_job(id),
+						primary key(userid,jobid));
