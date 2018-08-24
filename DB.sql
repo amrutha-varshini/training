251ee135a9ef97c2b13c97a8545f82652dd80a79
amrutha-varshini <40825488+amrutha-varshini@users.noreply.github.com>
2018-08-24 20:28:18 +0530

    adds DB

--- DB.sql
+++ DB.sql
@@ -1,6 +1,6 @@
 create database mini_care;
 use mini_care;
-create table users(id int(255) not null auto_increment,
+create table users(id int(255) auto_increment,
 					role varchar(255),
 					fname varchar(255),
 					lname varchar(255),
@@ -15,22 +15,21 @@ create table users(id int(255) not null auto_increment,
 					last_active timestamp,
 					status enum("ACTIVE","INACTIVE"),
 					primary key(id));
-
-create table seeker_job(id int(255) not null auto_increment,
+create table seeker_job(id int(255) auto_increment,
 					userID int(255)  ,
 					title varchar(255)  ,
 					descr varchar(255)  ,
 					foreign key(userID) references users(id),
 					primary key(id));
 					
-create table sitter(id int(255) not null, 
+create table sitter(id int(255), 
 					title varchar(255),
 					descr varchar(255),
 					foreign key(id) references users(id),
 					primary key(id));
 	
-create table job_status(jobid int(255) not null,
-						userid int(255) not null,
+create table job_status(jobid int(255),
+						userid int(255),
 						foreign key(userid) references users(id),
 						foreign key(jobid) references seeker_job(id),
 						primary key(userid,jobid));
